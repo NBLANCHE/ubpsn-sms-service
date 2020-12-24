d64614eee3f6fcd2d7c1173561c0253a916d783b
Matthew Silverstein <matpsilver@gmail.com>
2020-12-24 14:00:47 -0500

    Fixed typo

--- newsletter_topic/models.py
+++ newsletter_topic/models.py
@@ -6,12 +6,14 @@ from django.db import models
 
 class NewsletterTopic(models.Model):
     name = models.CharField(max_length=255)
+
     def __str__(self):
-    	return self.name
+        return self.name
 
 
 class NewsletterTopicMessage(models.Model):
     message = models.CharField(max_length=255)
     newsletter_topic = models.ForeignKey(NewsletterTopic, on_delete=models.CASCADE, related_name='messages')
-    def __str__(self),
-    	return self.message
+
+    def __str__(self):
+        return self.message
