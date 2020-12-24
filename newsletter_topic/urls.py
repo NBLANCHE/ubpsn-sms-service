7edf6639a78de8b3ec101295d022bb64a8ebc405
terenceli <terence.tch.li@gmail.com>
2020-12-24 13:16:12 -0600

    allow for more specific urls to be used with router

--- newsletter_topic/urls.py
+++ newsletter_topic/urls.py
@@ -4,15 +4,10 @@ from rest_framework.routers import DefaultRouter
 from newsletter_topic.views import NewsletterTopicViewSet, NewsletterTopicMessageViewSet
 
 router = DefaultRouter()
-
-
 router.register(r'topic', NewsletterTopicViewSet)
 router.register(r'topic_message', NewsletterTopicMessageViewSet)
 urlpatterns = router.urls
-# urlpatterns = [
-#     path('', views.NewsletterTopicList.as_view()),
-#     path('<int:pk>/', views.NewsletterTopicDetail.as_view()),
-#     path('<int:pk>/message/', views.NewsletterTopicMessageList.as_view())
-# ]
+urlpatterns = [
+]
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += router.urls
