f847d4e7944a10c6a5c671a60ce5d7c8bf95b132
Terence Li <terence.tch.li@gmail.com>
2020-12-31 10:39:20 -0600

    add stock logging

--- newsletter_topic/views.py
+++ newsletter_topic/views.py
@@ -3,13 +3,14 @@ from django.http import Http404
 # Create your views here.
 from rest_framework import status, generics, viewsets
 from rest_framework.response import Response
+import logging
 
 from newsletter_topic.models import NewsletterTopic, NewsletterTopicMessage
 from newsletter_topic.serializers import (
     NewsletterTopicSerializer,
     NewsletterTopicMessageSerializer,
 )
-
+logger = logging.getLogger(__name__)
 
 class NewsletterTopicViewSet(viewsets.ModelViewSet):
     queryset = NewsletterTopic.objects.all()
@@ -21,4 +22,4 @@ class NewsletterTopicMessageViewSet(viewsets.ModelViewSet):
     serializer_class = NewsletterTopicMessageSerializer
     # Get users subscribed, then call service
     def perform_create(self, serializer):
-        pass
+        logger.info(__name__)
